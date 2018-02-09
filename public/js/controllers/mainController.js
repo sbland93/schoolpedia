cc4172c59df50d665ae96a51cae8ce13203526ca
hyh4827 <hyh4827@naver.com>
2018-02-09 17:52:00 +0900

    mainBar

--- public/js/controllers/mainController.js
+++ public/js/controllers/mainController.js
@@ -65,5 +65,62 @@ $(document).ready(function(){
 			}
 		});
 	});
+	var searchedSchoolList = TPL.NFsearchedSchools;
+	var searchedProfileList = TPL.NFsearchedProfileList;
+	
+	$(".searchSchoolForm").validate({
+		rules:{
+			name:{
+				required:true,
+				minlength:1,
+			},
+
+		},
+		messages:{
+			name:"한글자 이상입니다.",
+		},
+		submitHandler:function(form,evt){
+			evt.preventDefault();
+			var sendingData = $(form).serialize();
+			
+			getSchools(sendingData).then(function(data){
+				console.log(data);
+				if(data.length) return $('#mainSearchResult').html(searchedSchoolList({searchedList : data}));
+				if(!data.success) return $('#mainSearchResult').html(searchedSchoolList({noData : true}));
+			});
+		}
+	})
+
+	$(".searchProfileForms").validate({
+		rules:{
+			name:{
+				required:true,
+				minlength:1,
+				maxlength:5,
+			}
+		},
+		messages:{
+			name:"한글자 이상입니다.",
+		},
+		submitHandler:function(form,evt){
+			evt.preventDefault();
+			var sendingData = $(form).serialize();
+			getProfiles(sendingData).then(function(data){
+				var defaultSchoolId = $('#schoolId').val();
+				if(data.length){
+					return $('#mainSearchedProfileListTPL').html(searchedProfileList({
+						searchedList : data,
+						schoolId : defaultSchoolId, 
+					}));
+				} 
+				if(!data.success) return $('#mainSearchedProfileListTPL').html(searchedProfileList({
+					noData : true,
+					schoolId : defaultSchoolId, 
+				}));
+			}).catch(function(err){
+				alert(err);
+			});
+		}
+	})
 
 })
